ecd6162dfc8d5557f9b3e582fa6e42b0113c3173
souvikrajsingh <souvikrajsingh@gmail.com>
2024-02-21 12:57:56 +0530

    nav-bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,23 +8,21 @@ function Navbar() {
   return (
     <div>
         <header className='bg-white'>
-            <nav className='flex justify-between items-center w-[92%] mx-auto pt-4 font-montserat'>
+            <nav className='flex justify-between items-center w-[92%] mx-auto pt-6 font-montserat'>
                 <div>
                     <span className = "font-bold"><a href="http://localhost:5173/">Souvik</a></span>
                 </div>
                 <div>
-                    <ul className='flex justify-center gap-10'>
+                    <ul className='flex justify-center gap-10 py-2'>
                         <li><a href="#">Projects</a></li>
                         <li><a href="#">Blogs</a></li>
                         <li><a href="#" className='flex'>Resume <MdOutlineSimCardDownload className="h-6" /></a></li>
-                        <li></li>
                     </ul>
                 </div>
-                {/* <div className = 'flex '>
-                <FaSearch/><input className="pl-1"placeholder="Type to search"/>
-                </div> */}
-                <div>                    
-                    <button className='h-10'><MdDarkMode/></button>
+                <div>
+                    <button className='bg-black hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full' >Get in Touch</button>                    
+                    <button className='h-10 pl-10'><MdDarkMode/></button>
+                    
                 </div>
             </nav>
         </header>
