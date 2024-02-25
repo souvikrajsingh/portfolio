66bb4d28475bfac1927c5fe7767b9c11133cab09
souvikrajsingh <souvikrajsingh@gmail.com>
2024-02-26 00:00:24 +0530

    next-ui-config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+
+const { nextui } = require("@nextui-org/react");
 export default {
   content: [
     "./index.html",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
@@ -11,6 +14,8 @@ export default {
     },
     extend: {},
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
+
 }
 
